2109326115dbae01cbe73b4414ac06e43c5b290e
Yugandhar <nanda.yugandhar@gmail.com>
2025-02-20 16:54:15 +0530

    Create page_view_example.dart
    PageView(
      controller: <PageController>,
      onPageChanged: (index) {},
      children: [<page view widget>], // all pages
    ),
    
    <PageController>.animateToPage(...) will animate to page.

---
+++ page_view_example.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class TestContainer extends StatelessWidget {
+  final Color color;
+  final String text;
+
+  TestContainer({required this.text, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    // learning colored box always takes max space
+    return Center(
+      child: Container(
+        color: color,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// we have controllers which are ChangeNotifiers
+// They track some value (text, page, tab, scroll, animation state) and can notify the listeners
+class PageControllerExample extends StatefulWidget {
+  _PageControllerState createState() => _PageControllerState();
+}
+
+class _PageControllerState extends State<PageControllerExample> {
+  // learning
+  // 1. put as controller in PageView
+  // 2. use this controller to navigate, _pageController.animateToPage(...) method
+  // 3. unlike TabController, this doesn't take care of buttons / trigger logic, call the method manually, based on event (button click, swipe...)
+  PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _gotoPreviousPage() {
+    if (_currentPage > 0) {
+      _currentPage--;
+    }
+    _pageController.animateToPage(
+      _currentPage,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void _gotoNextPage() {
+    if (_currentPage < 2) {
+      _currentPage++;
+    }
+    _pageController.animateToPage(
+      _currentPage,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {},
+              children: [
+                TestContainer(color: Colors.red, text: "Page 1"),
+                TestContainer(color: Colors.green, text: "Page 2"),
+                TestContainer(color: Colors.blue, text: "Page 3"),
+              ],
+            ),
+          ),
+        ],
+      ),
+
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            onPressed: _gotoPreviousPage,
+            child: Icon(Icons.arrow_back),
+          ),
+          FloatingActionButton(
+            onPressed: _gotoNextPage,
+            child: Icon(Icons.arrow_forward),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ColumnListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: 100,
+            itemBuilder:
+                (context, index) => Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                  child: Text(
+                    "Item ${index + 1}",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: Scaffold(body: PageControllerExample()),
+    );
+  }
+}
