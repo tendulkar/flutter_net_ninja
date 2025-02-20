cd343e151de8f8ae091e84f72c8ac1213b3116a5
Yugandhar <nanda.yugandhar@gmail.com>
2025-02-20 17:57:55 +0530

    Create scroll_controller_example.dart
    1. ScrollController _controller = ScrollController();
    2. _controller.addListerner() to handle scrolling events for user. 
    3. _controller.animateTo(position, duration, animation curve)
    4. _controller.position.maxScrollExtent is position of end of scrolling (whatsapp). 
    5. _controller.position.userScrollDirection defines direction of user scrolling.

---
+++ scroll/scroll_controller_example.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class ScrollControllerExample extends StatefulWidget {
+  @override
+  _ScrollControllerExampleState createState() => _ScrollControllerExampleState();
+}
+
+class _ScrollControllerExampleState extends State<ScrollControllerExample> {
+  ScrollController _controller = ScrollController();
+  bool _isFabVisible = true;
+  
+  void _scrollToBottom() {
+    _controller.animateTo(
+      _controller.position.maxScrollExtent,
+      duration: Duration(seconds: 1),
+      curve: Curves.easeOut,
+    );
+  }
+  
+  
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      if (_controller.position.userScrollDirection == ScrollDirection.reverse) {
+        setState(() { _isFabVisible = false;});
+      } else if (_controller.position.userScrollDirection == ScrollDirection.forward) {
+        setState(() { _isFabVisible = true;});
+      }
+    });
+  }
+  
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Scroll example")),
+      body: ListView.builder(
+        controller: _controller,
+        itemCount: 50,
+        itemBuilder: (context, index) => ListTile(title: Text("Item ${index + 1}")),
+      ),
+      floatingActionButton: _isFabVisible ? FloatingActionButton(
+        onPressed: _scrollToBottom,
+        child: Icon(Icons.arrow_downward),
+      ): null,
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: Scaffold(body: ScrollControllerExample()),
+    );
+  }
+}
